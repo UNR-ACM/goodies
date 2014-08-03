f75cab83dedcab500369a03a7110f337dfeaf981
Terence Henriod <thenriod@gmail.com>
2014-08-02 21:15:21 -0700

    Added a script that will set a background

---
+++ set_preferences.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script will set your Ubuntu theme settings for you
+# Just set the variables to appropriate values, change your terminal to
+# the directory with this file with the cd command and run:
+#	bash set_preferences.sh
+
+#--------   Variables   -------------
+pictureUrl="http://www.kucuu.com/desktop-wallpaper-home/Black-Background-Metal-1080P-hd-wallpapers-hd-wallpaper-1920x1440-13-507f977a064c7-8556.jpg"
+
+pictureFileName="Black-Background-Metal-1080P-hd-wallpapers-hd-wallpaper-1920x1440-13-507f977a064c7-8556.jpg"
+#------------------------------------
+
+cd ~/Pictures
+
+wget $pictureUrl
+
+dir=$(pwd)
+gsettings set org.gnome.desktop.background picture-uri file://$dir/$pictureFileName
+
+
+gsettings set org.gnome.shell.background color %#000000
+
